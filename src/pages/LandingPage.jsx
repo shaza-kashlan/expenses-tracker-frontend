3cff9cf0c309b27829ff1dae9a3b82efeda36841
Adrian Doonan <adriandoonan@gmail.com>
2024-04-05 14:22:11 +0200

    basic landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 const LandingPage = () => {
+	const navigate = useNavigate();
 	return (
-		<>
+		<div id="landing-page-content">
 			<h1>just another expense tracker</h1>
-		</>
+			<h2>Money stuff we good</h2>
+			<div
+				className="parallax"
+				style={{ backgroundImage: 'url("cash1.avif")' }}
+			/>
+			<h2>track give and get money</h2>
+			<div
+				className="parallax"
+				style={{ backgroundImage: 'url("cash2.avif")' }}
+			/>
+			<h2>import your money movements</h2>
+			<div
+				className="parallax"
+				style={{ backgroundImage: 'url("cash3.avif")' }}
+			/>
+			<button type="button" onClick={() => navigate("/signup")}>
+				Sign up
+			</button>
+		</div>
 	);
 };
 export default LandingPage;
