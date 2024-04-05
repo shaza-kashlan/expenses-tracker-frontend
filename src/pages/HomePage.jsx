51ff17b591bc3eb0a229db7a003aecc06da80ee2
shaza kashlan <eng.shazakashlan@gmail.com>
2024-04-05 11:57:48 +0200

    add transaltion files for auth flow

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { useTranslation } from "react-i18next";
 
 export const HomePage = () => {
   const { user, handleLogout } = useContext(AuthContext);
-
+  const { t } = useTranslation();
   return (
     <div>
       <h1>Welcome {user.userName}</h1>
 
-      <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleLogout}>{t("logout")}</button>
     </div>
   );
 };
