a927bbb1885ab7b8e2ce26aa5f05f20da2de2857
shaza kashlan <eng.shazakashlan@gmail.com>
2024-04-05 10:38:16 +0200

    add react-i18next to handle multi languages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
-import LanguageSelector from "./components/LanguageSelector";
+import ExampleTranslation from "./components/ExampleTranslation";
+import i18n from "../i18n";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // Change language to English when the app starts
+    i18n.changeLanguage("en");
+  }, []);
   return (
     <>
       <Routes>
-        <Route path="/" element={<LanguageSelector />} />
+        <Route path="/" element={<ExampleTranslation />} />
       </Routes>
     </>
   );
