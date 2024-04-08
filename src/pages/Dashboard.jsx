2396a6106e423d35a90322fbe5b1cc71653e729c
Adrian Doonan <adriandoonan@gmail.com>
2024-04-08 20:49:25 +0200

    put cards in a slider

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,47 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import WalletSummaryCard from '../components/WalletSummaryCard';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
+
 
 const Dashboard = ({data}) => {
 
   const wallets = Array.from(new Set(data.map(expense => expense.wallet)))
   return (
-    <div>
+    <>
         <h1>Dashboard</h1>
         <p>Got {data.length} bits of data</p>
 
-        {wallets.map(wallet => <WalletSummaryCard key={wallet} wallet={wallet} data={data.filter(expense => expense.wallet === wallet)} />)}
-        
-    </div>
+        <Splide 
+        aria-label="Your wallets"
+        options={{
+          type: "slide",
+          role: '',
+          gap: '1rem',
+          width: 400,
+          mediaQuery: 'min',
+          arrows: false,
+          breakpoints: {
+            412: {
+              width: 400
+            },
+            640: {
+              width: 600,
+              arrows: true,
+            },
+            960: {
+              width: 900,
+              arrows: true,
+              perPage: 2
+            },
+          }
+        }}>
+          {wallets.map(wallet => 
+            <SplideSlide key={wallet} >
+              <WalletSummaryCard wallet={wallet} data={data.filter(expense => expense.wallet === wallet)} />
+            </SplideSlide>)}
+        </Splide>
+    </>
   )
 }
 export default Dashboard
