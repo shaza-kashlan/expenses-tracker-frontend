7d040ef49b6d57de77e090f2306f0d9840052023
Adrian Doonan <adriandoonan@gmail.com>
2024-04-11 17:48:27 +0200

    try fix dashboard

--- src/components/WalletSummaryCard.jsx
+++ src/components/WalletSummaryCard.jsx
@@ -4,6 +4,10 @@ import dayjs from "dayjs"
 import { useTranslation } from "react-i18next";
 
 const WalletSummaryCard = ({data, wallet}) => {
+  if (data.length === 0) { 
+    return
+  }
+  console.log('wall card data', data, 'wallet card wallet',wallet)
   const { t } = useTranslation();
   const totalBalance = data.reduce((acc,expense) => acc + expense.amount,0)
 
@@ -14,23 +18,25 @@ const WalletSummaryCard = ({data, wallet}) => {
   const navigate = useNavigate()
 
   //console.log('got this data',data)
+ 
+
+    return (
+      <article className="wallet-summary-card">
+        <header>
+          <h2>{t(`${wallet}`)}</h2> 
+          <p className="align-right">{totalBalance.toFixed(2)} €</p>
+          </header>
+        <section>
+        <p>{t("You've had")} <strong>{data.length}</strong>{t(" transactions with this wallet")} </p>
+        <p className="push-apart">{t("Account first used:")}<span>{opened}</span></p>
+        <p className="push-apart">{t("Last used:")} <span>{lastUsed}</span></p>
+        </section>
+        <footer>
+          <button type="button" className="button-small" onClick={() => navigate(`/sources/660e9ba097cea6ff1b929df4/expenses`)} >{t("View transactions")}</button>
+          <button type="button" className="button-small" onClick={() => navigate(`/sources/660e9ba097cea6ff1b929df4`)} >{t("Edit this wallet")}</button>
+        </footer>
+      </article>
+    )
   
-  return (
-    <article className="wallet-summary-card">
-      <header>
-        <h2>{t(`${wallet}`)}</h2> 
-        <p className="align-right">{totalBalance.toFixed(2)} €</p>
-        </header>
-      <section>
-      <p>{t("You've had")} <strong>{data.length}</strong>{t(" transactions with this wallet")} </p>
-      <p className="push-apart">{t("Account first used:")}<span>{opened}</span></p>
-      <p className="push-apart">{t("Last used:")} <span>{lastUsed}</span></p>
-      </section>
-      <footer>
-        <button type="button" className="button-small" onClick={() => navigate(`/sources/660e9ba097cea6ff1b929df4/expenses`)} >{t("View transactions")}</button>
-        <button type="button" className="button-small" onClick={() => navigate(`/sources/660e9ba097cea6ff1b929df4`)} >{t("Edit this wallet")}</button>
-      </footer>
-    </article>
-  )
 }
 export default WalletSummaryCard
